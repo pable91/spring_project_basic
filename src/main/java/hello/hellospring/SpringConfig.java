7870b48bbded50fee140f0b5754393d33b18f313
KimYongKwon <dydrnjs518@gmail.com>
2021-03-10 22:51:10 +0900

    change to jdbc

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,31 @@
 package hello.hellospring;
 
+import hello.hellospring.Repository.JdbcMemberRepository;
 import hello.hellospring.Repository.MemberRepository;
 import hello.hellospring.Repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
 
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
-    public MemberService memeberService() {
+    public MemberService memberService() {
         return new MemberService(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+// return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
